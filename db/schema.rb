ad9b7a4f53963f90dccd96dfb375806d27b2f4e0
aqua25 <h.aqua25@gmail.com>
2019-02-22 18:53:42 +0900

    Add a lot

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_21_145608) do
+ActiveRecord::Schema.define(version: 2019_02_22_093939) do
 
   create_table "articles", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -64,6 +64,7 @@ ActiveRecord::Schema.define(version: 2019_02_21_145608) do
     t.string "family_name"
     t.string "true_name"
     t.text "profile"
+    t.text "profile_image"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
