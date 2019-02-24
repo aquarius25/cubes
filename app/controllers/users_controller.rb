472c59594d3751740fdbaea438149d1b99c9fa41
aqua25 <h.aqua25@gmail.com>
2019-02-25 03:07:39 +0900

    Delete no need migration files & changed views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,14 @@
 class UsersController < ApplicationController
 
   def index
+    @user = User.all
   end
 
   def new
   end
 
   def create
+
   end
 
   def edit
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_22_093939) do
+ActiveRecord::Schema.define(version: 2019_02_20_121239) do
 
   create_table "articles", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -60,10 +60,6 @@ ActiveRecord::Schema.define(version: 2019_02_22_093939) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "nickname"
-    t.string "family_name"
-    t.string "true_name"
-    t.text "profile"
-    t.text "profile_image"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
