c466171f2543388867a7631b4124a7c166425c8a
aqua25 <h.aqua25@gmail.com>
2019-02-24 10:06:04 +0900

    Write views 4 tweet function

--- db/migrate/20190219153607_create_tweets.rb
+++ db/migrate/20190219153607_create_tweets.rb
@@ -1,7 +1,6 @@
 class CreateTweets < ActiveRecord::Migration[5.2]
   def change
     create_table :tweets do |t|
-      t.string :name,	null: false
       t.text	:text, null: false
       t.string	:image, null: true
       t.integer :user_id,	null: false
