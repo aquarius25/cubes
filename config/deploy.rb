29c068f0b37d0339cb88973c94dd30da09e2a2f9
aqua25 <h.aqua25@gmail.com>
2019-02-27 16:04:15 +0900

    Changed git info

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock "~> 3.11.0"
 
 set :application, "cubes"
-set :repo_url, "git@github.com:aquarius25/cubes.git"
+set :repo_url, "git@github.com:1ZZaquarius25/cubes.git"
 
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
