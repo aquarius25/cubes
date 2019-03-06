5dfb3648ec8fbd55a27961606885cc8c96a6b7b3
aqua25 <h.aqua25@gmail.com>
2019-03-06 20:26:56 +0900

    User functions

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,10 +1,4 @@
 class ArticlesController < ApplicationController
-  before_action :login_or_not
   def index
-  end
-
-  private
-  def login_or_not
-    redirect_to new_user_session_path unless user_signed_in?
   end
 end
