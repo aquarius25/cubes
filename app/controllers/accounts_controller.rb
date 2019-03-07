01023b7e2afb9a19fc539bffdb4bbb9bc65405db
aqua25 <h.aqua25@gmail.com>
2019-03-08 05:03:06 +0900

    Profile views

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,5 +1,5 @@
 class AccountsController < ApplicationController
-
+  before_action :set_account, only: [:edit, :update]
   def index
   end
 
@@ -14,17 +14,25 @@ class AccountsController < ApplicationController
   end
 
   def show
-    @account = Account.find(params[:id])
   end
 
   def edit
   end
 
   def update
+    if @account.update(account_params)
+      redirect_to action: :index
+    else
+      render :edit
+    end
   end
 
   private
   def account_params
     params.require(:account).permit(:image, :f_name, :t_name, :profile, :link).merge(user_id: current_user.id)
+  end
+
+  def set_account
+    @account = Account.find(params[:id])
   end
 end
